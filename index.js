575ed2fc192de2c9aa0503c880beab36246de19b
sumit9235 <sy7985494155@gmail.com>
2023-08-03 20:19:29 +0530

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors=require('cors');
+const { chatRouter } = require('./routes/chat.route');
+require("dotenv").config()
+const app=express();
+app.use(cors());
+app.use(express.json());
+app.get("/",(req,res)=>{
+ res.send("Hello World")
+})
+app.use("/test",chatRouter)
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("Conected to server")
+})
